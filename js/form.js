8b28032f7c26254445e2bb1c1054f07c5cc7904b
Nadia Pillard <ua9ond@yandex.ru>
2017-03-31 03:15:10 +0700

    history-add creates

--- js/form.js
+++ js/form.js
@@ -23,9 +23,39 @@ $(document).ready(function() {
         };
         $.datepicker.setDefaults($.datepicker.regional['ru']);
     });
-    
+
     $('[data-valid=date]').datepicker();
 
     $('[data-valid=phone]').mask("+7 (999) 999-99-99");
+
+
+
+    $('[data-valid=upload]').change(function(){
+      var filename = $(this).val().replace(/.*\\/, "");
+      alert('eus');
+      var uploadItem = $(this).closest('.form__upload');
+      uploadItem.find('.form__upload-path').html(filename);
+      uploadItem.find('label').hide();
+      uploadItem.find('.form__upload-result').fadeIn();
+    });
+
+    /*$('.franchise-form__upload-remove').click(function(evt){
+      evt.preventDefault();
+      var self = $(this);
+      $('#popupDialogImg, #dialog-overlay').fadeIn();
+      $('#popupConfirmImg').click(function(evt) {
+        evt.preventDefault();
+        var currentWrp = self.closest('.franchise-form__upload-item');
+        currentWrp.find('.franchise-form__upload-wrp').show();
+        currentWrp.find('.franchise-form__upload-result').hide();
+        currentWrp.find('input[type=file]').val('');
+        $('#popupDialogImg, #dialog-overlay').fadeOut();
+      });
+
+      $('#popupCancelImg').click(function(evt) {
+        $('#popupDialogImg, #dialog-overlay').fadeOut();
+    });
+
+    })*/
 
 })
