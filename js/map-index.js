4498ada0b8616cbe5ff232b607bad6c7e9a6f72d
Nadia Pillard <ua9ond@yandex.ru>
2017-04-02 22:09:18 +0700

    index map done

--- js/map-index.js
+++ js/map-index.js
@@ -9,9 +9,48 @@ ymaps.ready(init);
             controls: []
         });
 
-        myPlacemark = new ymaps.Placemark([59.936985, 30.314880], {
-            hintContent: 'Сан-Галли',
-            balloonContent: 'Изобретатель русской батареи',
+        myPlacemark1 = new ymaps.Placemark([59.938732, 30.332286], {
+            hintContent: 'Русский музей',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-home.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+        myPlacemark2 = new ymaps.Placemark([59.934096, 30.324385], {
+            hintContent: 'Казанский собор',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-home.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+        myPlacemark3 = new ymaps.Placemark([59.934204, 30.306703], {
+            hintContent: 'Исаакиевский собор',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-home.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+        myPlacemark4 = new ymaps.Placemark([59.941023, 30.313271], {
+            hintContent: 'Эрмитаж',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-home.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+        myPlacemark5 = new ymaps.Placemark([59.925506, 30.295589], {
+            hintContent: 'Эрмитаж',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-home.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+        myPlacemark6 = new ymaps.Placemark([59.949503, 30.316570], {
+            hintContent: 'Первопавловская крепость',
         }, {
             iconLayout: 'default#image',
             iconImageHref: 'img/map/tooltip-home.svg',
@@ -19,5 +58,65 @@ ymaps.ready(init);
             iconImageOffset: [-93, -76]
         });
 
-        //myMap.geoObjects.add(myPlacemark);
+
+        myPlacemark7 = new ymaps.Placemark([59.927527, 30.348057], {
+            hintContent: '',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-map.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+
+        myPlacemark8 = new ymaps.Placemark([59.934804, 30.348229], {
+            hintContent: 'Литейный проспект',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-pointer.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+
+        myPlacemark9 = new ymaps.Placemark([59.941253, 30.357885], {
+            hintContent: '',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-man.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+
+        myPlacemark10 = new ymaps.Placemark([59.938497, 30.272140], {
+            hintContent: '',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-note.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+
+        myPlacemark11 = new ymaps.Placemark([59.936964, 30.303008], {
+            hintContent: '',
+        }, {
+            iconLayout: 'default#image',
+            iconImageHref: 'img/map/tooltip-pointer.svg',
+            iconImageSize: [93, 76],
+            iconImageOffset: [-93, -76]
+        });
+
+
+
+
+        myMap.geoObjects
+        .add(myPlacemark11)
+        .add(myPlacemark1)
+        .add(myPlacemark2)
+        .add(myPlacemark3)
+        .add(myPlacemark4)
+        .add(myPlacemark5)
+        .add(myPlacemark6)
+        .add(myPlacemark7)
+        .add(myPlacemark8)
+        .add(myPlacemark9)
+        .add(myPlacemark10);
     }
