484890119a4cd9139b34df7e1f8988c10315e5eb
Nadia Pillard <pillard.nadia@gmail.com>
2017-06-01 12:34:37 +0200

    list-pages added

--- gulpfile.js
+++ gulpfile.js
@@ -102,7 +102,7 @@ gulp.task("min-js", function() {
 /**
  * Push build to gh-pages
  */
-gulp.task('deploy', function () {
+gulp.task('deploy', ['build'], function () {
   return gulp.src("build/**/*")
     .pipe(deploy())
 });
